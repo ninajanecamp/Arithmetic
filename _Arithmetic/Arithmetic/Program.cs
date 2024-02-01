a560f076b1797bb823ff0d1a077803391beeafe4
unknown <Admin@CSLAB247.CSSERVER.local>
2024-02-01 20:06:17 +0800

    arithmeticsss

---
+++ _Arithmetic/Arithmetic/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Arithmetic
+{
+
+    public class Math
+    {
+
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multi(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public int Div(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        class program
+        {
+
+            static void Main(string[] args)
+            {
+                Math math = new Math();
+
+                int num1, num2;
+
+                //addition
+                Console.WriteLine("Enter num1:");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter num2:");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("The total of two numbers  = " + math.Add(num1,num2));
+
+                //subtraction
+                Console.WriteLine("Enter num1:");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter num2:");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("The total of two numbers = " + math.Sub(num1, num2));
+
+                //multiplication
+                Console.WriteLine("Enter num1:");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter num2:");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("The total of two numbers = " + math.Multi(num1, num2));
+
+                //division
+                Console.WriteLine("Enter num1:");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter num2:");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("The total of two numbers = " + math.Div(num1, num2));
+            }
+
+        }
+    }
+
+
+}
---
+++ _Arithmetic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Arithmetic
+{
+    public class Math {
+
+        public int Add(num1, num2)
+        {
+            return num1 + num2;
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Math math = new Math();
+
+                int num1, num2;
+
+                Console.WriteLine("Enter num1:");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter num2:");
+                num2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("total = " + math.Add(num1,num2));
+            }
+        }
+    }
+    
+}
